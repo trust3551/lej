ea530023a37443340df7ae9838cfde9a3527875d
trust3551 <trust317@naver.com>
2017-12-12 09:48:56 +0900

    1

---
+++ app/src/main/java/com/example/a507/lej/MyReceiverSendSms.java
@@ -0,0 +1,35 @@
+package com.example.a507.lej;
+
+/**
+ * Created by USER on 2017-11-14.
+ */
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.SmsManager;
+import android.widget.Toast;
+
+public class MyReceiverSendSms extends BroadcastReceiver {
+    int i;
+    public MyReceiverSendSms() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        // throw new UnsupportedOperationException("Not yet implemented");
+        String sMsg;
+        switch (getResultCode()) {
+            case Activity.RESULT_OK :
+                sMsg = "Success!" ; break;
+            case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                sMsg = "Failed!";
+                break;
+            default: sMsg = "Unknown error.";
+        }
+        Toast.makeText(context, sMsg, Toast.LENGTH_SHORT).show();
+    }
+}
